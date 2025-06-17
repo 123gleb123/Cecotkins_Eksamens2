483d88b04ebfb97b67e30e67c6e5faaef5aa6cea
123gleb123 <gchechotkin@gmail.com>
2025-06-17 20:10:38 +0300

    Pabeidzu taisit uzd 1 un saku taisit uzd 2

---
+++ uzd1.py
@@ -0,0 +1,6 @@
+# Lietotājs ievada skaitli
+number = int(input("Ievadi skaitli: "))
+# Cikls, kas iet cauri skaitļiem no 1 līdz 10
+for i in range(1, 11):
+    # Izdrukā reizināšanas tabulas rindu
+    print(f"{number} x {i} = {number * i}")
