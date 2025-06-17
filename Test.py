7bd34437047b57e1a44734e33ac183b27b79171a
123gleb123 <gchechotkin@gmail.com>
2025-06-18 02:32:47 +0300

    Veica jautājumus, atbilžu variantus, pareizās atbildes, kā arī papildināja mācību materiālu

---
+++ Test.py
@@ -0,0 +1,95 @@
+jautajumi = [
+    {
+        "jautajums": "Ko dara cikls 'for' Python valodā?",
+        "varianti": [
+            "Izpilda kodu vienu reizi",
+            "Atkārto kodu noteiktu reižu skaitu",
+            "Pastāvīgi pārbauda nosacījumu",
+            "Darbojas bezgalīgi"
+        ],
+        "atbilde": 1
+    },
+    {
+        "jautajums": "Kurš atslēgvārds tiek izmantots, lai sāktu 'for' ciklu Python valodā?",
+        "varianti": ["loop", "while", "for", "repeat"],
+        "atbilde": 2
+    },
+    {
+        "jautajums": "Kurā gadījumā 'for' cikls neko neizpildīs?",
+        "varianti": [
+            "for i in range(0):",
+            "for i in range(1):",
+            "for i in range(2):",
+            "for i in range(5):"
+        ],
+        "atbilde": 0
+    },
+    {
+        "jautajums": "Kas notiks, izpildot šo kodu?\n\nfor i in range(3):\n    if i == 1:\n        break\n    print(i)",
+        "varianti": [
+            "0 1 2",
+            "1 2",
+            "0",
+            "0 1"
+        ],
+        "atbilde": 2
+    },
+    {
+        "jautajums": "Ko izvadīs: for i in range(3): print(i)?",
+        "varianti": ["0 1 2", "1 2 3", "0 1 2 3", "3 2 1"],
+        "atbilde": 0
+    },
+    {
+        "jautajums": "Cik reizes tiks izpildīts cikls?\n\nfor i in range(4):\n    print(\"Hello\")",
+        "varianti": [
+            "3",
+            "4",
+            "5",
+            "0"
+        ],
+        "atbilde": 1
+    },
+    {
+        "jautajums": "Kas tiks izvadīts?\n\nfor i in range(5):\n    if i % 2 == 0:\n        continue\n    print(i, end=' ')",
+        "varianti": [
+            "1 2 3 4",
+            "0 2 4",
+            "1 3",
+            "0 1 2 3 4"
+        ],
+        "atbilde": 2
+    },
+    {
+        "jautajums": "Kurš no šiem variantiem korekti izmanto 'for' ciklu?",
+        "varianti": [
+            "for i to 10:",
+            "for i < 10:",
+            "for i in range(10):",
+            "for range i (10):"
+        ],
+        "atbilde": 2
+    },
+    {
+        "jautajums": "Kas notiek, ja 'for' ciklā tiek izmantots 'break'?",
+        "varianti": [
+            "Turpina ciklu",
+            "Iziet no cikla",
+            "Izlaiž vienu iterāciju",
+            "Izraisa kļūdu"
+        ],
+        "atbilde": 1
+    },
+    {
+        "jautajums": "Ko dara 'continue' ciklā 'for'?",
+        "varianti": [
+            "Pāriet uz nākamo iterāciju",
+            "Apstādina ciklu",
+            "Restartē ciklu",
+            "Beidz programmu"
+        ],
+        "atbilde": 0
+    }
+]
+
+lietotaja_atb = []
+
