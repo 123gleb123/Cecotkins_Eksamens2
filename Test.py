7962ed8dbefb9544e1d43554807fc44d96a614d7
123gleb123 <gchechotkin@gmail.com>
2025-06-18 03:12:22 +0300

    Uztaisiju menu un info logi

--- Test.py
+++ Test.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox
+
 jautajumi = [
     {
         "jautajums": "Ko dara cikls 'for' Python valodā?",
@@ -91,5 +94,40 @@ jautajumi = [
     }
 ]
 
-lietotaja_atb = []
 
+class Test:
+
+    def menu(self):
+        self.clear_window()
+        tk.Label(self.root, text="Python programmēšanas valodas for cikla tests", font=("Arial", 20, "bold"),
+                 fg="white", bg="#2E2E2E").pack(pady=20)
+
+        tk.Button(self.root, text="Sākt pildit tesu", command=self.play_game, font=("Arial", 14, "bold"),
+                  bg="#1F1F1F", fg="white", padx=20, pady=10).pack(pady=15)
+
+        tk.Button(self.root, text="Par tesu", command=self.par_testu, font=("Arial", 14, "bold"),
+                  bg="#5E4B8B", fg="white", padx=20, pady=10).pack(pady=15)
+
+        tk.Button(self.root, text="Iziet", command=self.root.quit, font=("Arial", 14, "bold"),
+                  bg="#D32F2F", fg="white", padx=20, pady=10).pack(pady=15)
+
+    def par_testu(self):
+        self.clear_window()
+        tk.Label(self.root, text="Par testu", font=("Arial", 18, "bold"), fg="white", bg="#2E2E2E").pack(pady=20)
+
+        test_info = (
+            "Šis tests paredzēts, lai pārbaudītu zināšanas par for cikla \n"
+            "darbību Python programmēšanas valodā. Jautājumi aptver\n"
+            "sintakses pamatus, darbu ar range() funkciju, kā arī break un\n"
+            "continue operatoru izmantošanu."
+        )
+
+        tk.Label(self.root, text=test_info, font=("Arial", 14), fg="white", bg="#2E2E2E", wraplength=500, justify="left").pack(pady=15)
+
+        tk.Button(self.root, text="Atgriezties uz galveno izvēlni", command=self.menu,
+                  font=("Arial", 14, "bold"), bg="#388E3C", fg="white", padx=20, pady=10).pack(pady=10)
+
+
+root = tk.Tk()
+app = Test(root)
+root.mainloop()
